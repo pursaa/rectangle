070af9691eaa9a4b109a1b395bf10a525bf72a39
Debbie Michel and Safiy Cham <debbie+safiy@>
2017-09-11 11:11:22 -0700

    separate business logic

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require('sinatra')
 require('sinatra/reloader')
-require('pry')
+# require('pry')
+also_reload('.lib/**/*.rb')
+require('./lib/rectangle')
 
 get('/') do
   erb(:input)
@@ -8,12 +10,18 @@ end
 
 get('/output') do
   @length = params.fetch("length")
-
   @width = params.fetch("width")
-  if @length == @width
-    @rectangle = "This is a square."
-  else
-    @rectangle = "This is not a square"
-  end
+  # if @length == @width
+  #   @rectangle = "This is a square."
+  # else
+  #   @rectangle = "This is not a square"
+  # end
+    rectangle = Rectangle.new(@length, @width)
+    if rectangle.square?
+      @string_to_display = "This is a square."
+    else
+      @string_to_display = "This is not a square."
+    end
+
 erb(:output)
 end
