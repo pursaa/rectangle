90739bb39eafdc43450d8a099aa68344683d86bd
Debbie Michel and Safiy Cham <debbie@>
2017-09-11 10:27:18 -0700

    Add a test file

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require('sinatra')
+require('sinatra/reloader')
+require('pry')
 
-get('/rectangle') do
-  "This application will determine whether a rectangle is a square or not."
+get('/') do
+  erb(:input)
+end
+
+get('/output') do
+  @length = params.fetch("length")
+
+  @width = params.fetch("width")
+  if @length == @width
+    @rectangle = "This is a square."
+  else
+    @rectangle = "This is not a square"
+  end
+erb(:output)
 end
